a537d7f4f53a4bc28e1f70e241adb9985a1bb475
DJames <davejames1109@gmail.com>
2025-11-12 23:35:00 +0800

    Add customer validation group

--- zephyr/customer-service/src/main/java/org/castle/djames/dto/CustomerRequest.java
+++ zephyr/customer-service/src/main/java/org/castle/djames/dto/CustomerRequest.java
@@ -2,14 +2,15 @@ package org.castle.djames.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.castle.djames.validator.groups.AddCustomerGroup;
 
 import java.time.LocalDate;
 
 public record CustomerRequest(
-        @NotBlank String firstName,
-        @NotBlank String lastName,
-        @NotBlank LocalDate birtDate,
-        @NotBlank @Email String email,
-        @NotBlank String phone,
-        @NotBlank String address) {
+        @NotBlank(groups = {AddCustomerGroup.class}) String firstName,
+        @NotBlank(groups = {AddCustomerGroup.class}) String lastName,
+        @NotBlank(groups = {AddCustomerGroup.class}) LocalDate birtDate,
+        @NotBlank(groups = {AddCustomerGroup.class}) @Email String email,
+        @NotBlank(groups = {AddCustomerGroup.class}) String phone,
+        @NotBlank(groups = {AddCustomerGroup.class}) String address) {
 }
---
+++ zephyr/customer-service/src/main/java/org/castle/djames/validator/groups/AddCustomerGroup.java
@@ -0,0 +1,4 @@
+package org.castle.djames.validator.groups;
+
+public interface AddCustomerGroup {
+}
