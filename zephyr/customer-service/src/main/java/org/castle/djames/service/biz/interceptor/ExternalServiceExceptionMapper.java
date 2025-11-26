eee0b0fac155c84f03caa3b223ed96974f6b4b25
DJames <davejames1109@gmail.com>
2025-11-26 11:19:23 +0800

    Add external service exception mapper

---
+++ zephyr/customer-service/src/main/java/org/castle/djames/service/biz/interceptor/ExternalServiceExceptionMapper.java
@@ -0,0 +1,22 @@
+package org.castle.djames.service.biz.interceptor;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.rest.client.ext.ResponseExceptionMapper;
+
+@Slf4j
+@Provider
+public class ExternalServiceExceptionMapper implements ResponseExceptionMapper<RuntimeException> {
+
+
+    @Override
+    public RuntimeException toThrowable(Response response) {
+        if (response.getStatus() >= 400) {
+            log.error("External service returned error response: {}", response);
+            return new RuntimeException("External service returned error response"); // return custom exception
+        }
+
+        return null;
+    }
+}
